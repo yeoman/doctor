4bb62a81f62064b9f4dd892c31328c3ee98a9451
Jelte Lagendijk <jwlagendijk@gmail.com>
2014-12-17 19:37:10 -0800

    Start making rules asynchronous and template error messages

--- test/rule-global_config.js
+++ test/rule-global_config.js
@@ -3,7 +3,14 @@
 var assert = require('assert');
 var sinon = require('sinon');
 var fs = require('fs');
+var path = require('path');
 var rule = require('../lib/rules/global_config');
+
+// Setting the message paths & files before fs is stubbed
+var messageSyntaxPath = path.join(__dirname, '../lib/messages', 'global-config-syntax.twig');
+var messageSyntaxFile = fs.readFileSync(messageSyntaxPath, 'utf8');
+var messageMiscPath = path.join(__dirname, '../lib/messages', 'global-config-misc.twig');
+var messageMiscFile = fs.readFileSync(messageMiscPath, 'utf8');
 
 describe('global config rule', function () {
   beforeEach(function () {
@@ -26,14 +33,23 @@ describe('global config rule', function () {
   });
 
   it('fails if JSON is invalid', function () {
-    this.sandbox.stub(fs, 'existsSync').returns(true);
-    this.sandbox.stub(fs, 'readFileSync').returns('@#');
+    this.sandbox.stub(fs, 'existsSync').withArgs(rule.configPath).returns(true);
+
+    var fsStub = this.sandbox.stub(fs, 'readFileSync');
+    fsStub.withArgs(rule.configPath).returns('@#');
+    fsStub.withArgs(messageSyntaxPath).returns(messageSyntaxFile);
+
     assert.equal(rule.verify(), rule.errors.syntax(new SyntaxError('Unexpected token @'), rule.configPath));
   });
 
   it('fails if file is unreadable', function () {
-    this.sandbox.stub(fs, 'existsSync').returns(true);
-    this.sandbox.stub(fs, 'readFileSync').throws(new Error('nope'));
+    this.sandbox.stub(fs, 'existsSync').withArgs(rule.configPath).returns(true);
+
+    var fsStub = this.sandbox.stub(fs, 'readFileSync');
+    fsStub.withArgs(rule.configPath).throws(new Error('nope'));
+    fsStub.withArgs(messageMiscPath).returns(messageMiscFile);
+
     assert.equal(rule.verify(), rule.errors.misc(rule.configPath));
   });
+
 });
