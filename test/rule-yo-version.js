0f5a83be6e6684a638cc8e405801474ca040a592
LitoMore <litomore@gmail.com>
2019-04-15 23:26:01 +0800

    Update test

--- test/rule-yo-version.js
+++ test/rule-yo-version.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const proxyquire = require('proxyquire');
 
 describe('yo version', () => {
-  const latestVersion = {
+  let latestVersion = {
     catch() {
       return latestVersion;
     }
@@ -15,18 +15,14 @@ describe('yo version', () => {
   });
 
   it('pass if it\'s new enough', async () => {
-    latestVersion.then = callback => {
-      callback('1.8.4');
-    };
+    latestVersion = {...latestVersion, then: cb => cb('1.8.4')};
 
     const error = await rule.verify();
     assert(!error, error);
   });
 
   it('fail if it\'s too old', async () => {
-    latestVersion.then = callback => {
-      callback('999.999.999');
-    };
+    latestVersion = {...latestVersion, then: cb => cb('999.999.999')};
 
     const error = await rule.verify();
     assert(error, error);
