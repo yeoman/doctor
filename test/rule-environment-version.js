61998a4d4b6d675e2ff9b3118f1a63a9f5c925d4
Marcelo Shima <marceloshima@gmail.com>
2025-08-15 19:34:40 -0300

    feat: add environment version check

---
+++ test/rule-environment-version.js
@@ -0,0 +1,26 @@
+
+import assert from 'node:assert';
+import rule from '../lib/rules/environment-version.js';
+
+describe('environment version', () => {
+  it('pass if it\'s new enough', async () => {
+    rule.latestVersion = () => Promise.resolve('1.8.4');
+
+    const error = await rule.verify();
+    assert.ok(!error, error);
+  });
+
+  it('fail if it\'s too old', async () => {
+    rule.latestVersion = () => Promise.resolve('999.999.999');
+
+    const error = await rule.verify();
+    assert.ok(error, error);
+  });
+
+  it('fail if it\'s invalid version range', async () => {
+    rule.latestVersion = () => Promise.resolve('-1');
+
+    const error = await rule.verify();
+    assert.ok(error, error);
+  });
+});
