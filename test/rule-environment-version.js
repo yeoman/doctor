a09e4ceeee8df4c1b5782e537e13f627d9ae9f35
Marcelo Shima <marceloshima@gmail.com>
2025-09-25 18:42:20 -0300

    chore: lint

--- test/rule-environment-version.js
+++ test/rule-environment-version.js
@@ -1,11 +1,11 @@
 
 import assert from 'node:assert';
-import { esmocha } from 'esmocha';
+import {esmocha} from 'esmocha';
 
-const { default: binaryVersion } = await esmocha.mock('binary-version', { default: esmocha.fn() });
-const { default: latestVersion } = await esmocha.mock('latest-version', { default: esmocha.fn() });
+const {default: binaryVersion} = await esmocha.mock('binary-version', {default: esmocha.fn()});
+const {default: latestVersion} = await esmocha.mock('latest-version', {default: esmocha.fn()});
 
-const { default: rule } = await import('../lib/rules/environment-version.js');
+const {default: rule} = await import('../lib/rules/environment-version.js');
 
 describe('environment version', () => {
   beforeEach(() => {
@@ -13,12 +13,12 @@ describe('environment version', () => {
   });
 
   it('pass if it\'s new enough', async () => {
-    // mock installed yo
+    // Mock installed yo
     binaryVersion.mockResolvedValueOnce('6.0.0');
 
-    // mock latest yeoman-environment
+    // Mock latest yeoman-environment
     latestVersion.mockResolvedValueOnce('1.8.4');
-    // mock installed yeoman-environment
+    // Mock installed yeoman-environment
     binaryVersion.mockResolvedValueOnce('2.0.0');
 
     const error = await rule.verify();
@@ -26,12 +26,12 @@ describe('environment version', () => {
   });
 
   it('fail if it\'s too old', async () => {
-    // mock installed yo
+    // Mock installed yo
     binaryVersion.mockResolvedValueOnce('6.0.0');
 
-    // mock latest yeoman-environment
+    // Mock latest yeoman-environment
     latestVersion.mockResolvedValueOnce('999.999.999');
-    // mock installed yeoman-environment
+    // Mock installed yeoman-environment
     binaryVersion.mockResolvedValueOnce('2.0.0');
 
     const error = await rule.verify();
@@ -39,12 +39,12 @@ describe('environment version', () => {
   });
 
   it('fail if it\'s invalid version range', async () => {
-    // mock installed yo
+    // Mock installed yo
     binaryVersion.mockResolvedValueOnce('6.0.0');
 
-    // mock latest yeoman-environment
+    // Mock latest yeoman-environment
     latestVersion.mockResolvedValueOnce('-1');
-    // mock installed yeoman-environment
+    // Mock installed yeoman-environment
     binaryVersion.mockResolvedValueOnce('2.0.0');
 
     const error = await rule.verify();
