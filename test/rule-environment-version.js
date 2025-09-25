35ac5571f02ca4b7d749d0c78c87de5cb8af1583
Marcelo Shima <marceloshima@gmail.com>
2025-09-25 18:40:09 -0300

    chore: adjust test

--- test/rule-environment-version.js
+++ test/rule-environment-version.js
@@ -1,24 +1,51 @@
 
 import assert from 'node:assert';
-import rule from '../lib/rules/environment-version.js';
+import { esmocha } from 'esmocha';
+
+const { default: binaryVersion } = await esmocha.mock('binary-version', { default: esmocha.fn() });
+const { default: latestVersion } = await esmocha.mock('latest-version', { default: esmocha.fn() });
+
+const { default: rule } = await import('../lib/rules/environment-version.js');
 
 describe('environment version', () => {
+  beforeEach(() => {
+    esmocha.resetAllMocks();
+  });
+
   it('pass if it\'s new enough', async () => {
-    rule.latestVersion = () => Promise.resolve('1.8.4');
+    // mock installed yo
+    binaryVersion.mockResolvedValueOnce('6.0.0');
+
+    // mock latest yeoman-environment
+    latestVersion.mockResolvedValueOnce('1.8.4');
+    // mock installed yeoman-environment
+    binaryVersion.mockResolvedValueOnce('2.0.0');
 
     const error = await rule.verify();
     assert.ok(!error, error);
   });
 
   it('fail if it\'s too old', async () => {
-    rule.latestVersion = () => Promise.resolve('999.999.999');
+    // mock installed yo
+    binaryVersion.mockResolvedValueOnce('6.0.0');
+
+    // mock latest yeoman-environment
+    latestVersion.mockResolvedValueOnce('999.999.999');
+    // mock installed yeoman-environment
+    binaryVersion.mockResolvedValueOnce('2.0.0');
 
     const error = await rule.verify();
     assert.ok(error, error);
   });
 
   it('fail if it\'s invalid version range', async () => {
-    rule.latestVersion = () => Promise.resolve('-1');
+    // mock installed yo
+    binaryVersion.mockResolvedValueOnce('6.0.0');
+
+    // mock latest yeoman-environment
+    latestVersion.mockResolvedValueOnce('-1');
+    // mock installed yeoman-environment
+    binaryVersion.mockResolvedValueOnce('2.0.0');
 
     const error = await rule.verify();
     assert.ok(error, error);
